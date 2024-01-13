8bd9d87e49f00e6de7875cb28d137b3c2d69cee7
qxl0 <qxl0@yahoo.com>
2024-01-13 10:37:49 -0500

    Time: 492 ms (89.77%), Space: 27.8 MB (41.30%) - LeetHub

--- 3002-maximum-size-of-a-set-after-removals/3002-maximum-size-of-a-set-after-removals.py
+++ 3002-maximum-size-of-a-set-after-removals/3002-maximum-size-of-a-set-after-removals.py
@@ -4,8 +4,4 @@ class Solution:
         set1,set2 = set(nums1),set(nums2)
         both = set1&set2
         only1,only2 = set1-both,set2-both
-        p1, p2= min(n//2,len(only1)), min(n//2, len(only2))
-        if p1+p2>=n: return n
-        c = len(both)
-        if p1+p2 + c>=n: return n
-        return p1+p2+c
+        return min(n, min(n//2,len(only1))+min(n//2,len(only2))+len(both))
