d70101349598811644ceaa26050e2faf78c29854
qxl0 <qxl0@yahoo.com>
2024-01-13 17:10:33 -0500

    Time: 1304 ms (75.95%), Space: 48.8 MB (87.34%) - LeetHub

--- 3004-maximum-subtree-of-the-same-color/3004-maximum-subtree-of-the-same-color.py
+++ 3004-maximum-subtree-of-the-same-color/3004-maximum-subtree-of-the-same-color.py
@@ -6,27 +6,18 @@ class Solution:
         for u,v in edges:
             nxt[u].append(v)
             nxt[v].append(u)
-        def dfs(node,parent): # return: max(color,#ofnodes) among tree rooted at node
-            nonlocal ans            
-            ret = defaultdict(int)
-            for nei in nxt[node]:
-                if nei == parent: continue
-                clr,num = dfs(nei,node)
-                ret[clr]+=num
-            # check if there is only one clr
-            node_color = colors[node]
-             
-            if  len(ret)==1 and node_color in ret:  # best case
-                ans = max(ans, ret[node_color] + 1)
-                return node_color, ret[node_color]+1
-            else:
-                if len(ret)==0:
-                    ans = max(ans,1)
-                    return node_color,1
+        def dfs(node,parent): # return: (current cnt,global cnt)
+            maxNodes,cur = -1,1
+            for v in nxt[node]:
+                if v == parent: continue
+                c,cnt = dfs(v, node)
+                if c!=-1 and colors[v]==colors[node] and cur!=-1:
+                    cur += c
                 else:
-                    return 0,0
-        dfs(0,-1)
-        return ans 
+                    cur = -1
+                maxNodes = max(maxNodes, cnt)
+            return cur,max(cur,maxNodes)
+        return dfs(0, -1)[1]
                     
                 
             
