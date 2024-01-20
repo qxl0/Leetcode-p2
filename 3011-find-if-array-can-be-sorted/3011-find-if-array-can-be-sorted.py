8a0dc9fbce3acfc5f7e27989181558f6cb059eae
qxl0 <qxl0@yahoo.com>
2024-01-20 14:41:10 -0500

    Time: 72 ms (100.00%), Space: 16.5 MB (100.00%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        prevc = None        
+        curmax = 0
+        prevmax = -inf
+        for i,x in enumerate(nums):
+            bc = x.bit_count()
+            if not prevc:
+                prevc = bc
+                curmax = x
+            elif bc == prevc:
+                curmax = max(curmax, x)
+                if x < prevmax: return False
+            else: # new section
+                if curmax>x: 
+                    return False
+                prevc = bc
+                prevmax = curmax # cache prev max 
+                curmax = x
+        return True
+                
+            
+            
+        
