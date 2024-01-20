3a12adfa1aa5afed3ce79f6d607c5fae0ff68483
qxl0 <qxl0@yahoo.com>
2024-01-20 15:13:37 -0500

    Time: 85 ms (50.00%), Space: 16.6 MB (100.00%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -1,27 +1,19 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        n = len(nums)
-        
-        prevc = None        
-        curmax = 0
-        prevmax = -inf
-        for i,x in enumerate(nums):
-            bc = x.bit_count()
-            if not prevc:
-                prevc = bc
-                curmax = x
-            elif bc == prevc:
-                curmax = max(curmax, x)
-                if x < prevmax: return False
-            else: # new section
-                prevmax = curmax # cache prev max 
-                curmax = x
-                if x<prevmax: 
-                    return False
-                prevc = bc
+        lastgrouphigh = 0
+        high = nums[0]
+        bitchange = False
+        for i in range(1,len(nums)):
+            # check if cross group??
+            bitchange = (nums[i].bit_count()!=high.bit_count())
+            if bitchange:
+                lastgrouphigh = high
+            # maintain high
+            high = max(high, nums[i])
+            # check if not sortable 
+            if nums[i]<lastgrouphigh:
+                return False
+        return True 
+
+
                 
-        return True
-                
-            
-            
-        
