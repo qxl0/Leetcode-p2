7e63fa0d0ee8386ec1fa2b65ae5725eaa440eed5
qxl0 <qxl0@yahoo.com>
2024-01-27 18:21:01 -0800

    Time: 1288 ms (82.41%), Space: 36 MB (67.51%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.py
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.py
@@ -1,39 +1,13 @@
-from sortedcontainers import SortedDict
 class Solution:
-    def longestValidSubstring(self, word: str, forbidden: List[str]) -> int: 
-        fset = set()
-        def slide(word,length,Map):
-            n = len(word)
-            code = 0
-            for i,ch in enumerate(word):
-                if i>=length:
-                    code &= (1<<(5*(length-1)))-1 # 0000011....1 
-                code = (code<<5) + ord(ch)-ord('a')+1
-                if i>=length-1:
-                    if code in fset:
-                        if i-length+1 not in Map:
-                            Map[i-length+1] = []
-                        Map[i-length+1].append(i)            
-        
-        for fword in forbidden:
-            # get its code and stored set 
-            code = 0
-            for ch in fword:
-                code <<=5
-                code += ord(ch)-ord('a')+1
-            fset.add(code)
-        # scan word to check (i,i+l) is in fset
-        Map = SortedDict()
-        for l in range(1,11):
-            slide(word,l,Map)
-        
-        # <---
+    def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
+        fset = set(forbidden)
         n = len(word)
         ret = 0
-        rightBound = n
-        for i in range(n-1,-1,-1):
-            if i in Map:
-                for j in Map[i]:
-                    rightBound = min(rightBound, j)
-            ret = max(ret, rightBound-i)
-        return ret
+        left = 0
+        for i in range(n):
+            for j in range(max(i-10,left),i+1):
+                if word[j:i+1] in fset:
+                    left = j+1
+            ret = max(ret, i-left+1)
+        return ret 
+                
