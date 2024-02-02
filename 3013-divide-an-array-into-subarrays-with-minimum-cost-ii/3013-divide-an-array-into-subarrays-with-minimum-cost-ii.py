58e7b1682bfe3ae7f9c55ec713fb3d621b02c1b1
qxl0 <qxl0@yahoo.com>
2024-02-02 07:13:58 -0500

    Time: 1552 ms (72.46%), Space: 32 MB (42.14%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -1,19 +1,26 @@
 from sortedcontainers import SortedList
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        sl = SortedList(nums[1:dist+1])
+        """
+        window size: dist+1
+        goal: choose smallest k-1 items, nums[0] is fixed 
+        """
         n = len(nums)
-        cur,ret = sum(sl[:k-2]),float('inf')
+        sl = SortedList(nums[1:dist+1])  # dist elements into sl
+        ret, cur = float('inf'),sum(sl[:k-2]) # sum up k-2 elements 
         for i in range(dist+1,n):
             if sl.bisect(nums[i])<=k-2:
-                cur+=nums[i]
+                cur += nums[i]
             else:
-                cur+=sl[k-2]
+                cur += sl[k-2]
             ret = min(ret, cur)
             sl.add(nums[i])
+            # remove i-dist
             if sl.bisect(nums[i-dist])<=k-2:
                 cur -= nums[i-dist]
             else:
                 cur -= sl[k-2]
             sl.remove(nums[i-dist])
-        return ret+nums[0]
+        return ret +nums[0]
+            
+        
