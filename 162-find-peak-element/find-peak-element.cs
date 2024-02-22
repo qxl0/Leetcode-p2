25ccefd7473112d000a0e19c7f28f8f97a071c53
qxl0 <qxl0@yahoo.com>
2024-02-21 20:05:43 -0500

    Time: 71 ms (43.19%) | Memory: 41.2 MB (51.72%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int FindPeakElement(int[] nums) {
+        for (int i=0;i<nums.Length-1;i++) {
+            if (nums[i]>nums[i+1]) {
+                return i;
+            }
+        }
+
+        return nums.Length-1;
+    }
+}
