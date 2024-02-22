3f16e521a7b5347e3b6da1866ea2f1a064c1c5c5
qxl0 <qxl0@yahoo.com>
2024-02-21 20:41:41 -0500

    Time: 110 ms (21.46%) | Memory: 46.8 MB (54.50%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cs
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cs
@@ -1,14 +1,14 @@
 public class Solution {
     Dictionary<char,List<string>> Map = new Dictionary<char,List<string>>()
         {
-            {'2', new List<string>(){"a","b","c"}},
-            {'3', new List<string>(){"d","e","f"}},
-            {'4', new List<string>(){"g","h","i"}},
-            {'5', new List<string>(){"j","k","l"}},
-            {'6', new List<string>(){"m","n","o"}},
-            {'7', new List<string>(){"p","q", "r","s"}},
-            {'8', new List<string>(){"t","u","v"}},
-            {'9', new List<string>(){"w","x","y", "z"}}
+            ['2'] =new List<string>(){"a","b","c"},
+            ['3'] =new List<string>(){"d","e","f"},
+            ['4'] =new List<string>(){"g","h","i"},
+            ['5'] =new List<string>(){"j","k","l"},
+            ['6' ]=new List<string>(){"m","n","o"},
+            ['7' ]=new List<string>(){"p","q", "r","s"},
+            ['8']=new List<string>(){"t","u","v"},
+            ['9']= new List<string>(){"w","x","y", "z"}
         };
 
     List<string> ret = new List<string>();
