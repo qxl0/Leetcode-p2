11d385e1638894c1849af964c33fddc9f13302d7
qxl0 <qxl0@yahoo.com>
2024-02-22 20:17:38 -0500

    Time: 75 ms (36.37%) | Memory: 43.3 MB (11.20%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cs
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cs
@@ -1,14 +1,22 @@
 public class Solution {
+    int[] _cost;
+    Dictionary<int,int> memo = new();
     public int MinCostClimbingStairs(int[] cost) {
-        int n = cost.Length;
-        int[] dp = new int[n+1];
+        this._cost = cost;
+        return helper(cost.Length);
+    }
 
-        dp[0] = 0;
-        dp[1] = 0;
-        for (int i=2;i<n+1;i++) {
-            dp[i] = Math.Min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+    private int helper(int idx) {
+        if (idx<=1) 
+            return 0;
+        if (memo.ContainsKey(idx)) {
+            return memo[idx];
         }
 
-        return dp[n];
+        int downOne = _cost[idx-1] + helper(idx-1);
+        int downTwo = _cost[idx-2] + helper(idx-2);
+
+        memo[idx] = Math.Min(downOne,downTwo);
+        return memo[idx];
     }
 }
