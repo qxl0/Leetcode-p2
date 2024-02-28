de91a6893c8dec3d682fddd0a3fa09290230faa6
qxl0 <qxl0@yahoo.com>
2024-02-27 20:54:54 -0500

    Time: 375 ms (98.50%) | Memory: 83.2 MB (8.28%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cs
+++ 739-daily-temperatures/daily-temperatures.cs
@@ -3,22 +3,15 @@ public class Solution {
         int n = temperatures.Length;
         int[] nextLarger = new int[n];
         Array.Fill(nextLarger, n);
-
+        int[] ans = new int[n];
+        Array.Fill(ans, 0);
         Stack<int> stack = new();
         for (int i=0;i<n;i++) {
             while (stack.Count>0 && temperatures[stack.Peek()]<temperatures[i]) {
                 int top = stack.Pop();
-                nextLarger[top] = i;
+                ans[top] = i-top;
             }
             stack.Push(i);
-        }
-
-        int[] ans = new int[n];
-        for (int i=0;i<n;i++) {
-            if (nextLarger[i]!=n)
-                ans[i] = nextLarger[i]-i;
-            else
-                ans[i] = 0;
         }
 
         return ans;
