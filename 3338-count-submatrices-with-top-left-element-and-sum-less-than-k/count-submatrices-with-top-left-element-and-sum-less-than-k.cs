ee0b9d03ca42e4497abce9eee41969d9b21a7cce
qxl0 <qxl0@yahoo.com>
2024-03-03 11:58:29 -0500

    Time: 695 ms (100.00%) | Memory: 104.8 MB (100.00%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cs
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cs
@@ -1,28 +1,16 @@
 public class Solution {
     public int CountSubmatrices(int[][] grid, int k) {
-        int m = grid.Length, n = grid[0].Length;
+        int m=grid.Length,n=grid[0].Length;
+        int ret = 0;
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                grid[i][j] += (i>0?grid[i-1][j]:0) + (j>0?grid[i][j-1]:0) - ((i>0 && j>0 )? grid[i-1][j-1]:0);
 
-        int[][] presum = new int[m][];
-        for (int i=0;i<m;i++) {
-            presum[i] = new int[n];
-            for (int j=0;j<n;j++) {
-                presum[i][j] = (j>0? presum[i][j-1] : 0)+grid[i][j];
-            }
-        }        
-        for (int j=0;j<n;j++) {
-            for (int i=0;i<m;i++) {
-                presum[i][j] = (i>0? presum[i-1][j] : 0) + presum[i][j];
-            }
-        }        
-        
-        int ret = 0;        
-        for (int i=0;i<m;i++) {
-            for (int j=0;j<n;j++) {
-                if (presum[i][j]<=k) {
+                if (grid[i][j]<=k) 
                     ret += 1;
-                }        
             }
         }
+
         return ret;
     }
 }
