5a57572060437a3a00075ac4abf162983d5c71cf
qxl0 <qxl0@yahoo.com>
2024-03-11 15:37:42 -0400

    Time: 1911 ms (89.78%) | Memory: 63.6 MB (63.78%) - LeetSync

--- 3313-maximum-strength-of-k-disjoint-subarrays/maximum-strength-of-k-disjoint-subarrays.py
+++ 3313-maximum-strength-of-k-disjoint-subarrays/maximum-strength-of-k-disjoint-subarrays.py
@@ -1,29 +1,17 @@
 class Solution:
     def maximumStrength(self, nums: List[int], k: int) -> int:
+        """
+        dp[i][j]: max strength i subarrays, up to j index 
+        """
         n = len(nums)
-        dp = [[[-1,-1] for _ in range(k+1)] for _ in range(n)]
-        def helper(i,j,skip):
-            # return Max strength of j disjoint subarrays based on nums[i:] include nums[i] based on skip 
-            # x x x x x x x x x x x
-            # i
-            # nums[i], 
-            # j%2==1, add, otherwise minus 
-            if j<0: return -inf 
-            if (i==n):
-                if j==0: return 0
-                else: return -inf
-            if dp[i][j][skip]!=-1: return dp[i][j][skip]
+        dp = [[0]*(n+1) for _ in range(k+1)]
 
-            op1 = -inf 
-            op2 = -inf 
-            if skip:
-                op1 = helper(i+1,j,skip)
-            if j&1==1:
-                op2 = max(helper(i+1,j,0)+nums[i]*j, helper(i+1,j-1,1)+nums[i]*j)
-            else:
-                op2 = max(helper(i+1,j,0)-nums[i]*j, helper(i+1,j-1,1)-nums[i]*j)
-            
-            dp[i][j][skip] = max(op1,op2)
-            return dp[i][j][skip]
+        for i in range(1,k+1):
+            multiplier = k+1-i if i%2==1 else i-k-1
+            maxsum,cur = -inf,-inf
+            for j in range(i-1,n):
+                cur = max(cur+nums[j]*multiplier, dp[i-1][j]+nums[j]*multiplier)
+                maxsum = max(maxsum, cur)
+                dp[i][j+1] = maxsum
 
-        return max(helper(0,k,0),helper(0,k,1))
+        return dp[k][n]
