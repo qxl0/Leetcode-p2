dc8f6c18fe3b472c6d30312d486c21747491afe7
qxl0 <qxl0@yahoo.com>
2024-03-21 20:39:14 -0400

    Time: 796 ms (100.00%) | Memory: 18.8 MB (100.00%) - LeetSync

---
+++ 3393-make-string-anti-palindrome/make-string-anti-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def makeAntiPalindrome(self, s: str) -> str:
+        count = Counter(s)
+        n = len(s)
+        ans = sorted(ch for ch in s)
+        i=j=n//2 
+        while j<n and ans[j]==ans[i]:
+            j += 1
+        while ans[i]==ans[n-1-i]:
+            if j==n: return "-1"
+            ans[i],ans[j]=ans[j],ans[i]
+            i+=1
+            j+=1
+        
+        return "".join(ans)
+
+    
