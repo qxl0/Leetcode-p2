e9c444a17c187d865f0f75051e1483892e0353de
qxl0 <qxl0@yahoo.com>
2024-03-24 07:48:00 -0400

    Time: 554 ms (72.21%) | Memory: 27.1 MB (29.55%) - LeetSync

--- 3327-minimum-moves-to-pick-k-ones/minimum-moves-to-pick-k-ones.py
+++ 3327-minimum-moves-to-pick-k-ones/minimum-moves-to-pick-k-ones.py
@@ -1,41 +1,13 @@
 class Solution:
     def minimumMoves(self, nums: List[int], k: int, maxChanges: int) -> int:
-        """
-        x x x x x x x x x x , k, maxChanges=3
-                
-        """        
-        # [0, 1, 5, 6, 9]  k=3 maxChanges=1 
-        numstr = "".join(map(str, nums))
-        if "111" in numstr:
-            maxC = 3 
-        elif "11" in numstr:
-            maxC = 2
-        elif "1" in numstr:
-            maxC = 1
-        else:
-            maxC = 0
+        nums = [i for i, val in enumerate(nums) if val != 0]
+        n = len(nums)
+        prefix_sum = [0] + list(accumulate(nums))
+        res = inf
+        for m in range(max(0, k-maxChanges), min(n, max(0, k-maxChanges)+3, k) + 1):
+            for i in range(n - m + 1):
+                cur = (prefix_sum[i + m] - prefix_sum[i + m - m // 2]) - (prefix_sum[i + m // 2] - prefix_sum[i])
+                res = min(res, cur + (k - m) * 2)
+        return res
         
-        if maxC>k: maxC = k 
-        if maxChanges>=k-maxC:
-            return max(0,maxC-1) + 2*(k-maxC) 
-        
-        k -= maxChanges
-        res = maxChanges*2 
-
-        A = [i for i in range(len(nums)) if nums[i]==1]
-        mid = k//2
-        left = 0
-        cur = mn = sum(abs(i-A[mid]) for i in A[:k])
-        for right in range(k, len(A)):
-            # use O(1) to calculate cur when mid -> mid+1 by
-            # [left+1, ... mid, mid+1, ... right]
-            cur -= A[mid]-A[left]
-            left += 1
-            cur += A[right]-A[mid+1]
-            cur += ((mid+1-left)-(right-(mid+1)))*(A[mid+1]-A[mid])
-            mn = min(mn, cur)
-            mid +=1 
-        return res+mn
-        
-
         
