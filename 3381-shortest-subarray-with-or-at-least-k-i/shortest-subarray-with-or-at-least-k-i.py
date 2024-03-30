5920ae76107a8a7bf9fdad9c562173053b44739f
qxl0 <qxl0@yahoo.com>
2024-03-30 19:11:30 -0400

    Time: 76 ms (100.00%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.py
+++ 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.py
@@ -1,17 +1,34 @@
 class Solution:
-    def minimumSubarrayLength(self, nums: List[int], K: int) -> int:
-        ret = inf                 
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        ret = inf 
+        count = [0]*32
         n = len(nums)
-        def isspecial(start,end):
-            runningsum = 0
-            for k in range(start,end+1):
-                runningsum |= nums[k] 
-            return runningsum>=K 
-        for j in range(n):
-            for i in range(j,n):
-                if isspecial(j,i):
-                    ret = min(ret, i-j+1)
-        return ret if ret<inf else -1
+        def addtocount(x):
+            i=0
+            while i<32:
+                if (x>>i)&1==1:
+                    count[i]+= 1
+                i += 1
+        def removefromcount(x):
+            i=0
+            while i<32:
+                if (x>>i)&1==1:
+                    count[i]-= 1
+                i += 1
+        def getvalue(count):
+            ret = 0
+            for i in range(32):
+                if count[i]!=0:
+                    ret += 1<<i    
+            return ret 
         
-             
+        i  = 0
+        for j in range(n):
+            addtocount(nums[j])
+            if getvalue(count)<k:continue  
+            while i<=j and getvalue(count)>=k:
+                ret = min(ret, j-i+1)
+                removefromcount(nums[i])
+                i += 1
             
+        return ret if ret<inf else -1
