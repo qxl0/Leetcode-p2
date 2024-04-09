545924af5ba9c5e8ef093f67d69503d322d2879d
qxl0 <qxl0@yahoo.com>
2024-04-08 21:03:50 -0400

    Time: 533 ms (83.16%) | Memory: 26.6 MB (53.85%) - LeetSync

--- 3327-minimum-moves-to-pick-k-ones/minimum-moves-to-pick-k-ones.py
+++ 3327-minimum-moves-to-pick-k-ones/minimum-moves-to-pick-k-ones.py
@@ -1,13 +1,11 @@
 class Solution:
     def minimumMoves(self, nums: List[int], k: int, maxChanges: int) -> int:
-        nums = [i for i, val in enumerate(nums) if val != 0]
+        nums = [i for i in range(len(nums)) if nums[i]==1]
         n = len(nums)
-        prefix_sum = [0] + list(accumulate(nums))
-        res = inf
-        for m in range(max(0, k-maxChanges), min(n, max(0, k-maxChanges)+3, k) + 1):
-            for i in range(n - m + 1):
-                cur = (prefix_sum[i + m] - prefix_sum[i + m - m // 2]) - (prefix_sum[i + m // 2] - prefix_sum[i])
-                res = min(res, cur + (k - m) * 2)
-        return res
-        
-        
+        presum = list(accumulate(nums, initial=0))
+        ret = inf 
+        for m in range(max(0,k-maxChanges), min(k, max(0,k-maxChanges)+3)+1):
+            for i in range(n-m+1):
+                cur = presum[i+m]-presum[i+m-m//2]-(presum[i+m//2]-presum[i])
+                ret = min(ret, cur+(k-m)*2)
+        return ret 
