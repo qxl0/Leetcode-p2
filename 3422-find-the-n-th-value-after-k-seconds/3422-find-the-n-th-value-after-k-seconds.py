54fcd391e90ab8311d22e234bb46123ee1a3eb2b
qxl0 <qxl0@yahoo.com>
2024-06-09 08:03:33 -0400

    Time: 2344 ms (100%), Space: 17.2 MB (100%) - LeetHub

--- 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.py
+++ 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.py
@@ -1,13 +1,8 @@
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
-        MOD=10**9+7
         arr = [1]*n
-
         while k:
-            presum = list(accumulate(arr))
-            cur = [(arr[i]+(presum[i-1] if i>0 else 0))%MOD for i in range(n)]
-
-            arr = cur
-            k -= 1
-        
-        return arr[n-1]
+            k -=1
+            arr = list(accumulate(arr))
+        return arr[n-1]%(10**9+7)
+            
