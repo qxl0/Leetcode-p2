d254e33a808c75b0ef6aa61405d1457bfe8df582
qxl0 <qxl0@yahoo.com>
2024-06-12 20:50:18 -0400

    Time: 1422 ms (52.72%), Space: 24.6 MB (76.9%) - LeetHub

--- 3452-find-the-maximum-length-of-a-good-subsequence-ii/3452-find-the-maximum-length-of-a-good-subsequence-ii.py
+++ 3452-find-the-maximum-length-of-a-good-subsequence-ii/3452-find-the-maximum-length-of-a-good-subsequence-ii.py
@@ -1,22 +1,13 @@
 class Solution:
-    def maximumLength(self, nums: List[int], K: int) -> int:
-        n = len(nums)
-        dp = [[-1]*(K+1) for _ in range(n+1)]
-        
-        prevmax = [0]*n
-        for k in range(K+1):
-            Map = defaultdict(int)
-            curmax = [0]*n
-
-            for i in range(n-1,-1,-1):
-                dp[i][k] = max(1+Map[nums[i]], 1+ (prevmax[i+1] if i+1<n else 0) )
-                
-                Map[nums[i]] = max(Map[nums[i]], dp[i][k])
-                # save to Map   
-                curmax[i] = max(dp[i][k], (curmax[i+1] if i+1<n else 0))
-            prevmax = curmax 
-        ret = 0
-        for i in range(n):
-            ret = max(ret, dp[i][k])
-
-        return ret 
+    def maximumLength(self, nums: List[int], k: int) -> int:        
+        """
+        res[i]: max length of subsequence with i nonequals 
+        dp[i][a]: max length of subsequence ending in a with i nonequals  
+        """
+        dp = [Counter() for _ in range(k+1)]
+        res = [0]*(k+1)
+        for a in nums:
+            for i in range(k,-1,-1):
+                dp[i][a] = max(dp[i][a]+1, (res[i-1] if i>0 else 0)+1)
+                res[i] = max(res[i], dp[i][a])
+        return res[k]
