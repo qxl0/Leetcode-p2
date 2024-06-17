89e40ca883ffb0670a8cdf40fbaa5efc075ae85d
qxl0 <qxl0@yahoo.com>
2024-06-17 10:37:43 -0400

    Time: 538 ms (82.75%), Space: 51.6 MB (52.47%) - LeetHub

--- 3443-maximum-total-reward-using-operations-ii/3443-maximum-total-reward-using-operations-ii.cpp
+++ 3443-maximum-total-reward-using-operations-ii/3443-maximum-total-reward-using-operations-ii.cpp
@@ -2,17 +2,34 @@ class Solution {
 public:
     int maxTotalReward(vector<int>& A) {
         sort(A.begin(), A.end());
-        const int N = 100001;
+        auto last = unique(A.begin(),A.end());
+        A.erase(last,A.end());
+        const int N = 5 * 1e4+1;
         bitset<N> dp;
 
         dp[0] = 1;
-
-        for (int i=0;i<A.size();i++) {
+        // cout<<dp<<endl;
+        for (int i=0;i<A.size()-1;i++) {
             int val = A[i];
             int shift = dp.size()-val;
+            // dp: 1101011
+            // val: 4
+            // shift = 7-4 = 3
+            // dp<<shift = 1011000
+            // dp<<shift>>shift = 0001011 -> remove the top 3 digits 
+            // dp<<shift>>shift<<val = 10110000
             dp |= (dp<<shift>>shift<<val);            
         }
-        int pos = dp.to_string().find('1');
-        return dp.size()-pos-1;
+        // cout<<dp.size()<<endl;
+        int ret = 0;
+        for (int i=dp.size();i>=0; i--) {
+            if (dp[i]==1 && i<A.back()){
+                ret = i;
+                break;
+            }
+
+        }
+        // cout<<dp<<endl;
+        return ret+A.back();
     }
 };
