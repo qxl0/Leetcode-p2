06475e82d50b63d644dc6f1df2da18b682cfbf14
qxl0 <qxl0@yahoo.com>
2024-07-07 08:06:48 -0400

    Time: 451 ms (100%), Space: 31.7 MB (100%) - LeetHub

--- 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.py
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.py
@@ -1,5 +1,8 @@
 class Solution:
-    def maximumPoints(self, enemyEnergies: List[int], currentEnergy: int) -> int:        
+    def maximumPoints(self, enemyEnergies: List[int], currentEnergy: int) -> int:    
+        """ If you have at least 1 point, you can choose an unmarked enemy
+                                ^^^^, this is point, not energy 
+        """
         mn = min(enemyEnergies)
         if currentEnergy<mn: return 0
         sm = sum(enemyEnergies)-mn
