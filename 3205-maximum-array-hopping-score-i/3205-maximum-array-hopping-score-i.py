7c2a7b9d0762ef186437ea0450fc6e49edefae3d
qxl0 <qxl0@yahoo.com>
2024-07-06 20:26:37 -0400

    Time: 6575 ms (27.36%), Space: 16.8 MB (33.96%) - LeetHub

--- 3205-maximum-array-hopping-score-i/3205-maximum-array-hopping-score-i.py
+++ 3205-maximum-array-hopping-score-i/3205-maximum-array-hopping-score-i.py
@@ -1,13 +1,9 @@
 class Solution:
-    def maxScore(self, nums: List[int]) -> int:        
+    def maxScore(self, nums: List[int]) -> int:
         n = len(nums)
-        @cache
-        def dfs(i):                        
-            if i==n-1:
-                return 0
-            ret = 0
-            for j in range(i+1,n):
-                # i->j
-                ret = max(ret, (j-i)*nums[j]+dfs(j))
-            return ret 
-        return dfs(0) 
+        dp = [0]*n
+
+        for i in range(n):
+            for j in range(i):
+                dp[i] = max(dp[i], (i-j)*nums[i] + dp[j])
+        return dp[n-1]
