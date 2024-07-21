0670bc402a916cb6d3d509a179651817f43d673f
qxl0 <qxl0@yahoo.com>
2024-07-21 10:33:13 -0400

    Time: 1 ms (100%), Space: 41.7 MB (50%) - LeetHub

---
+++ 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String losingPlayer(int x, int y) {
+        // 75->x, 10->y
+        // A -> B -> A ...
+        // 115, 75 + 4*10
+        int turn = 0; // A
+        while (true) {
+            if (x>=1 && y>=4) {
+                turn = 1-turn;
+                x -= 1;
+                y -= 4;
+            }
+            else {
+                return turn==1? "Alice":"Bob";
+            }
+        }
+    }
+}
