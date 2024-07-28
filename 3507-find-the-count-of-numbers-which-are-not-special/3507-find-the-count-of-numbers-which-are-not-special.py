f581e29a298dba8dc24081b4f3b331171e708d75
qxl0 <qxl0@yahoo.com>
2024-07-28 16:28:36 -0400

    Time: 2183 ms (25%), Space: 16.8 MB (100%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.py
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.py
@@ -1,29 +1,22 @@
 class Solution:
     def nonSpecialCount(self, l: int, r: int) -> int:
-        def eratosthenes(n):
-            q = [0] * (n + 1)
-            primes = []
-
-            for i in range(2, int(sqrt(n)) + 1):
-                if q[i] == 1:
+        def getprimes(n):
+            dp = [0]*(n+1)
+            for i in range(2, int(sqrt(n))+1):
+                if dp[i]==1:
                     continue
-                j = i * 2
-                while j <= n:
-                    q[j] = 1
-                    j += i
-            for i in range(2, n + 1):
-                if q[i] == 0:
+                j = i*2
+                while j<=n:
+                    dp[j] = 1
+                    j += i          
+            primes = []  
+            for i in range(2,n+1):
+                if dp[i]==0:
                     primes.append(i)
             return primes
-
-        p = eratosthenes(int(sqrt(10**9)))
-
+        primes = getprimes(int(sqrt(10**9)))
         ret = 0
-        for x in p:
-            # if i==2: continue
-            if x>=sqrt(l) and x<=sqrt(r):
+        for p in primes:
+            if p>=sqrt(l) and p<=sqrt(r):
                 ret += 1
         return r-l+1-ret
-
-
-        
