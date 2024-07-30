3d54cff6c0dbba0330e39b76d8d2833398775311
qxl0 <qxl0@yahoo.com>
2024-07-30 06:57:55 -0400

    Time: 43 ms (8.14%), Space: 16.5 MB (62.18%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,24 +1,23 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        def findslp(s):
+        def findlsp(s):
             n = len(s)
             dp = [0]*n
-
             for i in range(1,n):
                 j = dp[i-1]
                 while j>0 and s[i]!=s[j]:
                     j = dp[j-1]
-                dp[i] = j + (1 if s[i]==s[j] else 0)
+                dp[i] = j+(1 if s[i]==s[j] else 0)
             return dp
-        lsp = findslp(needle)
+        lsp = findlsp(needle)
         m,n = len(needle),len(haystack)
-        dp = [0]*n
+        dp = [0]*n # dp[i]: length of max substring in haystack[:i] which is prefix of needle
         dp[0] = (1 if haystack[0]==needle[0] else 0)
         for i in range(1,n):
             j = dp[i-1]
             while j>0 and (j==m or haystack[i]!=needle[j]):
                 j = lsp[j-1]
-            dp[i] = j+(1 if haystack[i]==needle[j] else 0)
+            dp[i] = j + (1 if haystack[i]==needle[j] else 0)
         for i in range(n):
             if dp[i]==m:
                 return i-m+1
