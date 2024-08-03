58f8c83be950cbe36c009bb1e488a42f2307a21d
qxl0 <qxl0@yahoo.com>
2024-08-03 10:15:33 -0400

    Time: 15962 ms (14.36%), Space: 18.3 MB (32.44%) - LeetHub

--- 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.py
+++ 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.py
@@ -1,32 +1,37 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        n = len(s)  
-        ret = 0      
-        for k in range(1,int(sqrt(n))+1):
-            ones = 0
-            lastzero = -1
-            zeros = deque()
-            for right in range(n):
-                if s[right]=='0':
-                    zeros.append(right)
-                    while len(zeros)>k:
-                        ones -= (zeros[0]-lastzero-1)
-                        lastzero = zeros.popleft()                    
-                else:
-                    ones += 1
-                if len(zeros)==k and ones>=k**2:
-                    ret += min(ones-k**2+1, zeros[0]-lastzero)
-        i = 0
-        while i<n:
-            if s[i]=='0':
-                i += 1
-                continue
-            sz = 0
-            while i<n and s[i]=='1':
-                sz += 1
-                i += 1
-            ret += (sz*(sz+1))//2
+        n = len(s)
+        right = [0]*n
+        def findright():
+            right[n-1] = (1 if s[n-1]=='1' else 0)
+            for i in range(n-2,-1,-1):
+                if s[i]=='0':continue
+                right[i] = right[i+1] + (1 if s[i]=='1' else 0)
+        findright()
+        # print(right)
+        ret = 0
+        for m in range(1,int(sqrt(n))+1):
+            # m: # of 0 in the sliding window            
+            count = 0
+            j=0
+            for i in range(n):                                                           
+                while j<n and count<m:
+                    if s[j]=='0':
+                        count += 1
+                    j += 1
+                # when stop, x x x x 0]j
+                if count!=m: break  # we can't find m 0
+                one = j-i-m
+                if one+(right[j] if j<n else 0)>=count*count:
+                    extra = (right[j] if j<n else 0) - max(0,m*m-one)  # right[j]: # of 1 from j pos including j
+                    ret += max(0,extra + 1)
+                count -= (1 if s[i]=='0' else 0)
+        # m == 0
+        for i in range(n):
+            if s[i]=='0':continue
+            ret += right[i]
+        
+        return ret 
 
-        return ret
 
-                    
+
