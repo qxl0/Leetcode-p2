ea859fe00a3a9ed7e32c7dd43c635afb2ac86cad
qxl0 <qxl0@yahoo.com>
2024-08-02 20:07:01 -0400

    Time: 1362 ms (47.62%), Space: 42.8 MB (80.95%) - LeetHub

--- 3237-alt-and-tab-simulation/3237-alt-and-tab-simulation.py
+++ 3237-alt-and-tab-simulation/3237-alt-and-tab-simulation.py
@@ -1,28 +1,12 @@
 class Solution:
     def simulationResult(self, windows: List[int], queries: List[int]) -> List[int]:
-        Map1 = {val:i for i,val in enumerate(windows)}
-        Map2 = {i:val for i,val in enumerate(windows)}
-
-        idx = -1
-        for v in queries:  
-            i = Map1[v]          
-            del Map1[v]
-            Map1[v] = idx
-
-            del Map2[i]
-            Map2[idx] = v
-
-            idx -= 1
-            
-        n = len(windows)
         ret = []
-        poslst = sorted(Map1.values())
-
-        for idx in poslst:
-            ret.append(Map2[idx])
-        
+        seen = set()
+        for q in queries[::-1]:
+            if q not in seen:
+                seen.add(q)
+                ret.append(q)
+        for x in windows:
+            if x not in seen:
+                ret.append(x)
         return ret
-            
-
-
-
