20cd25ec45caf27ee2b7f85a366659b655060b46
qxl0 <qxl0@yahoo.com>
2024-08-13 20:51:02 -0400

    Time: 36 ms (48.96%), Space: 16.5 MB (64.89%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,14 +1,17 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         m,n = len(needle),len(haystack)
+
         def findlsp(s):
             n = len(s)
             dp = [0]*n
+
             for i in range(1,n):
                 j = dp[i-1]
                 while j>0 and s[i]!=s[j]:
                     j = dp[j-1]
-                dp[i] = j + (1 if s[i]==s[j] else 0)
+                # stop 
+                dp[i]=j + (1 if s[i]==s[j] else 0)
             return dp
         lsp = findlsp(needle)
         dp = [0]*n
@@ -17,8 +20,11 @@ class Solution:
             j = dp[i-1]
             while j>0 and (j==m or haystack[i]!=needle[j]):
                 j = lsp[j-1]
+            # stop 
             dp[i] = j + (1 if haystack[i]==needle[j] else 0)
+        
         for i in range(n):
             if dp[i]==m:
                 return i-m+1
         return -1
+        
