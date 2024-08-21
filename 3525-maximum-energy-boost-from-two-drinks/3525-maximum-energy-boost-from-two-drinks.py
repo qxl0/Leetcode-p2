e116de743e6390ab5080a981e7fb0d80acbc4768
qxl0 <qxl0@yahoo.com>
2024-08-21 07:29:17 -0400

    Time: 1451 ms (91.54%), Space: 33.9 MB (98.09%) - LeetHub

--- 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.py
+++ 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.py
@@ -2,14 +2,15 @@ class Solution:
     def maxEnergyBoost(self, energyDrinkA: List[int], energyDrinkB: List[int]) -> int:
         ret = 0
         n = len(energyDrinkA)
-        dp = [[0]*2 for _ in range(n+1)] 
+        dp0,dp1 = 0,0
         # dp[i][0]: max boost from ... len: i, last one is from A
         # dp[i][1]: max boost from ... len: i, last one is from B
         
         for i in range(1,n+1):
-            dp[i][0] = max(dp[i-1][0] + energyDrinkA[i-1], dp[i-1][1])
-            dp[i][1] = max(dp[i-1][1]+energyDrinkB[i-1], dp[i-1][0])
+            dp0_new = max(dp0+energyDrinkA[i-1], dp1)
+            dp1 = max(dp1+energyDrinkB[i-1], dp0)
+            dp0 = dp0_new
 
-        return max(dp[n])
+        return max(dp0,dp1)
             
         
