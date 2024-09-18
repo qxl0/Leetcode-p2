65cd2409a5c12c88218e77da6276677e7f9514e9
qxl0 <qxl0@yahoo.com>
2024-09-18 19:52:10 -0400

    Time: 37 ms (55.36%), Space: 16.6 MB (86.19%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.py
+++ 0726-number-of-atoms/0726-number-of-atoms.py
@@ -1,53 +1,49 @@
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        """
-        K4(ON(MgH3)2)2F  
-        """
-        def fill(ans,nam,tmp,cnt):
-            cnt = 1 if cnt==0 else cnt
-            if len(nam)>0:                        
-                ans[nam] += cnt
-            else:
-                for t in tmp.keys():
-                    ans[t] += tmp[t]*cnt
-        where = 0
         n = len(formula)
-        
+        where = 0
         def f(i):
             nonlocal where
             ans = defaultdict(int)
-            tmp = defaultdict(int)
-            cnt = 0
             nam = ''
-            while i < n and formula[i]!=')': 
-                if formula[i].isupper() or formula[i] == '(':
-                    fill(ans,nam,tmp,cnt)
-                    # set 
+            cnt = 0
+            pre = defaultdict(int)
+
+            while i<n and formula[i]!=')':
+                if formula[i].isupper() or formula[i]=='(':
+                    fill(ans, nam,pre,cnt)
                     nam = ''
+                    pre.clear()
                     cnt = 0
-                    tmp.clear()
                     if formula[i].isupper():
                         nam += formula[i]
                         i += 1
                     else:
-                        tmp = f(i+1)
+                        pre = f(i+1)
                         i = where + 1
-                elif formula[i].islower():
-                    nam += formula[i]
+                elif formula[i].isdigit():
+                    cnt = cnt*10 + ord(formula[i])-ord('0')
                     i += 1
                 else:
-                    cnt = cnt*10+ ord(formula[i])-ord('0')
+                    nam += formula[i]
                     i += 1
-            fill(ans,nam,tmp,cnt)
+            fill(ans, nam, pre, cnt)
             where = i
             return ans
-        res = f(0)        
-        output = []
-        for k in sorted(res.keys()):
-            output.append(k)
-            if res[k]>1:
-                output.append(str(res[k]))
-        return ''.join(output)
+        def fill(ans, nam, tmp, cnt):
+            cnt = 1 if cnt == 0 else cnt
+            if len(nam)>0:
+                ans[nam] += cnt
+            elif len(tmp)>0:
+                for k in tmp.keys():
+                    ans[k] = ans[k] + tmp[k]*cnt
+        ans = f(0)
+        ret = ''
+        for k in sorted(ans.keys()):
+            ret += k 
+            if ans[k]>1:
+                ret += str(ans[k])
+        return ret
 
 
-            
+                
