65f11b023264f6fdd3ff74d001d259326cef040b
qxl0 <qxl0@yahoo.com>
2024-11-17 10:03:59 -0500

    Time: 44 ms (100%), Space: 16.6 MB (100%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.py
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.py
@@ -1,34 +1,16 @@
 class Solution:
-    def countValidSelections(self, nums: List[int]) -> int:        
-        n = len(nums)
-        def isvalid(arr, start,dir):
-            # simulate 
-            cur = start 
-            while cur>=0 and cur<n:
-                if dir=='left':
-                    while cur>=0 and cur<n and arr[cur]==0:
-                        cur -= 1
-                else:
-                    while cur<n and cur>=0 and arr[cur]==0:
-                        cur += 1
-                # when stop, nums[cur]!=0
-                if cur<0 or cur>=n:
-                    if sum(arr)==0:
-                        return 1
-                    return 0
-                arr[cur]-=1
-                dir = 'right' if dir=='left' else 'left'
-                cur = cur + 1 if dir == 'right' else cur -1 
-            return 0
-        def valid(arr, start):
-            # return 0, 1, 2     
-            arr = nums.copy()       
-            ret = isvalid(arr, start,'left')
-            arr = nums.copy()
-            ret += isvalid(arr, start,'right')
-            return ret
-        pos = [i for i in range(n) if nums[i]==0]
+    def countValidSelections(self, nums: List[int]) -> int:
+        presum = list(accumulate(nums))
         ret = 0
-        for start in pos:            
-            ret += valid(nums, start)
+        n = len(nums)
+        for i in range(n):
+            if nums[i]!=0:
+                continue
+            # calculate left sum, and right sum
+            left = presum[i-1] if i>=1 else 0
+            right = presum[n-1]-presum[i]
+            if left==right:
+                ret += 2
+            elif abs(left-right)<=1:
+                ret += 1
         return ret
