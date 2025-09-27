30b1bed672ea329a151326202838321f4799e70b
qxl0 <qxl0@yahoo.com>
2025-09-27 08:09:35 -0400

    Time: 1192 ms (13.04%), Space: 54.7 MB (14.67%) - LeetHub

--- 0727-minimum-window-subsequence/0727-minimum-window-subsequence.py
+++ 0727-minimum-window-subsequence/0727-minimum-window-subsequence.py
@@ -1,29 +1,32 @@
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        slen, tlen = len(s),len(t)
-        dp = [[-1]*tlen for _ in range(slen)]
-        for i in range(slen):
-            if s[i] == t[0]:
-                dp[i][0] = i
-            else:
-                if i != 0:
-                    dp[i][0] = dp[i-1][0]
-        # dp
-        for i in range(1, slen):
-            for j in range(1, tlen):
-                if s[i] == t[j]:
-                    dp[i][j] = dp[i-1][j-1]
+    def minWindow(self, s1: str, s2: str) -> str:
+        m, n = len(s1), len(s2)
+        s1 = '#' + s1
+        s2 = '#' + s2
+
+        # dp[i][j]: minimum length k such that s2[1:j] is subsequence of s1[i-k+1:i] 
+        dp = [[inf]*(n+1) for _ in range(m+1)]
+        for i in range(m):
+            dp[i][0]=0
+        for j in range(n):
+            dp[0][j] = inf
+        dp[0][0] = 0
+        for i in range(1,m+1):
+            for j in range(1, n+1):
+                if s1[i]==s2[j]:
+                    dp[i][j] = dp[i-1][j-1] + 1
                 else:
-                    dp[i][j] = dp[i-1][j]
-        # print(dp)
-        # 
-        begin, minlen = -1, float('inf')
-        for i in range(slen):
-            if dp[i][tlen-1] != -1:
-                k = dp[i][tlen-1]
-                if k != -1:
-                    if minlen>i-k+1:
-                        begin,minlen = k, i-k+1
-        if begin == -1:
+                    dp[i][j] = dp[i-1][j] + 1
+        
+        # ans, dp[][n]  
+        l = inf
+        pos = 0
+        for i in range(1,m+1):
+            if dp[i][n] < l:
+                l = dp[i][n]
+                pos = i
+        if l == inf:
             return ""
-        return s[begin:begin+minlen]
+        return s1[pos-l+1:pos+1]
+                
+        
