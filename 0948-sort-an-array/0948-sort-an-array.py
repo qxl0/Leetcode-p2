20beab06c84504ec918d6500d3b1c0274c723095
qxl0 <qxl0@yahoo.com>
2025-10-11 10:28:46 -0400

    Time: 994 ms (6.02%), Space: 34.7 MB (5.35%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def quicksort(arr,l,r):
+            nonlocal first, last
+            if l>=r:
+                return arr
+            
+            x = nums[randint(l,r)]
+            partition(arr,l, r, x)
+            quicksort(arr,l,first-1)
+            quicksort(arr,last+1, r)
+            return arr
+        def partition(arr,l, r, x):
+            nonlocal first,last
+            i = l
+            first,last = l,r
+            while i <= last:
+                if nums[i] == x:
+                    i += 1
+                elif nums[i] < x:
+                    swap(nums, first, i)
+                    first += 1
+                    i += 1
+                else: # nums[i] > X
+                    swap(nums, i, last)
+                    last -= 1
+        def swap(arr, i, j):
+            tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+        first,last = -1,-1
+        return quicksort(nums,0,len(nums)-1)
+        
+        
