ee013d32617928c9fa99579c36a05b1f08ded441
qxl0 <qxl0@yahoo.com>
2025-10-12 09:48:59 -0400

    Time: 865 ms (13.62%), Space: 24.1 MB (85.8%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,34 +1,52 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def quicksort(arr,l,r):
-            nonlocal first, last
-            if l>=r:
-                return arr
-            
-            x = nums[randint(l,r)]
-            partition(arr,l, r, x)
-            quicksort(arr,l,first-1)
-            quicksort(arr,last+1, r)
-            return arr
-        def partition(arr,l, r, x):
+        def quicksort1(arr, l, r):
+            if l >= r:
+                return
+            x = arr[randint(l, r)] # [a, b) 
+            mid = partition1(arr, l, r, x)
+            quicksort1(arr, l, mid-1)
+            quicksort1(arr, mid+1, r)   
+            return arr         
+        def swap(arr, a, b):
+            tmp = arr[a]
+            arr[a] = arr[b]
+            arr[b] = tmp
+        def partition1(arr, l, r, x):
+            a = l
+            xi = 0
+            for i in range(l, r+1, 1):
+                if arr[i] <= x:
+                    swap(arr, a, i)
+                    if arr[a] == x:
+                        xi = a
+                    a += 1
+            swap(arr, xi, a-1)
+            return a-1
+        def quicksort2(arr, l, r):
             nonlocal first,last
+            if l >= r: return
+            x = arr[randint(l, r)]
+            partion2(arr, l, r, x)
+            left, right = first, last
+            quicksort2(arr, l, left-1)
+            quicksort2(arr, right+1, r)
+            return arr
+        def partion2(arr, l, r, x):
+            nonlocal first, last
+            first,last = l, r
             i = l
-            first,last = l,r
             while i <= last:
-                if nums[i] == x:
+                if arr[i] == x:
                     i += 1
-                elif nums[i] < x:
-                    swap(nums, first, i)
+                elif arr[i] < x:
+                    swap(arr, first, i)
                     first += 1
                     i += 1
-                else: # nums[i] > X
-                    swap(nums, i, last)
+                else:
+                    swap(arr, i, last)
                     last -= 1
-        def swap(arr, i, j):
-            tmp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = tmp
         first,last = -1,-1
-        return quicksort(nums,0,len(nums)-1)
-        
-        
+        if len(nums)>1:
+            return quicksort2(nums, 0, len(nums)-1)
+        return nums
