dee046d23384f57d0cd4e32a25379dd20fd4aafa
qxl0 <qxl0@yahoo.com>
2025-10-13 15:14:44 -0400

    Time: 7 ms (57.05%), Space: 19.3 MB (50.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        x, freq = count.most_common(1)[0]
-        return x
+        c,x = 0, None
+        for n in nums:
+            if x == n:
+                c += 1
+            elif c == 0:
+                c = 1
+                x = n
+            else:
+                c -= 1
+        if nums.count(x) >= len(nums)//2:
+            return x
+            
