6af70da8888388e87215acaf0df3dc7abd2a5489
qxl0 <qxl0@yahoo.com>
2025-10-25 10:59:56 -0400

    Time: 71 ms (61.61%), Space: 18.3 MB (53.11%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,17 +1,20 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        T = Counter(t)
-        S = Counter()
-        n = len(s)
-        j = 0
-        ans = s+"#"
-        for i in range(n):            
-            S[s[i]] += 1
-            while j <n and S >= T:
-                if len(ans)>len(s[j:i+1]):
-                    ans = s[j:i+1]
-                S[s[j]] -= 1
-                if S[s[j]] == 0:
-                    del S[s[j]]
-                j += 1
-        return ans if len(ans)<n+1 else ""
+        missing = Counter(t)
+        total = len(t)
+        if len(s)<total:
+            return ""
+        i = 0
+        I,J = 0,-1
+        for j in range(len(s)):
+            if missing[s[j]]>0:
+                total -= 1
+            missing[s[j]] -= 1
+            if total == 0:
+                while missing[s[i]]<0:
+                    missing[s[i]] += 1
+                    i += 1
+                if J<0 or j-i < J-I:
+                    I,J = i,j
+        return s[I:J+1] if J>=0 else ""
+            
