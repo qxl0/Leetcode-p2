68ce95f516c963e9d9a5d0f604cdc61657e48755
qxl0 <qxl0@yahoo.com>
2025-10-26 10:14:40 -0400

    Time: 140 ms (92.45%), Space: 31.8 MB (48.91%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,27 +1,28 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        MAXL = 100003
+        MAXL = 10**5+2
+        # MAXL = 9
         n = len(nums)
         dq = [0]*MAXL
         h=t=0
-        for i in range(k-1):  # add k-1 elements
-            while h<t and nums[dq[t-1]]<=nums[i]:
+        
+        # add k-1 elements to dq
+        for i in range(k-1):
+            while h<t and nums[dq[t-1]] < nums[i]:
                 t -= 1
             dq[t] = i
             t += 1
-        # start loop
-        j = 0
-        ans = [0]*(n-k+1)   
-        for i in range(k-1, len(nums)):
-            # add element at i
-            while h<t and nums[dq[t-1]]<=nums[i]:
+        # main loop
+        ans = [0]*(n-k+1)
+        for i in range(k-1, n):
+            # add i to dq
+            while h<t and nums[dq[t-1]] < nums[i]:
                 t -= 1
-            dq[t] = i            
+            dq[t] = i
             t += 1
-            # ans got 
+            # get ans
             ans[i-k+1] = nums[dq[h]]
-            # remove element at j
-            if dq[h]==j:
+            # remove element at i-k
+            if dq[h] == i-k+1:
                 h += 1
-            j += 1
         return ans
