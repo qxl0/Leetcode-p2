8e140c95d45e1039eacab9f152c970dea4246d29
qxl0 <qxl0@yahoo.com>
2025-11-05 19:52:11 -0500

    Time: 8687 ms (5.01%), Space: 18.8 MB (8.1%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,34 +1,22 @@
 class Solution:
-    def splitArray(self, nums: List[int], m: int) -> int:
+    def splitArray(self, nums: List[int], K: int) -> int:
         n = len(nums)
-        
-        # Create a prefix sum array of nums.
-        prefix_sum = [0] + list(itertools.accumulate(nums))
-        
-        @functools.lru_cache(None)
-        def get_min_largest_split_sum(curr_index: int, subarray_count: int):
-            # Base Case: If there is only one subarray left, then all of the remaining numbers
-            # must go in the current subarray. So return the sum of the remaining numbers.
-            if subarray_count == 1:
-                return prefix_sum[n] - prefix_sum[curr_index]
-        
-            # Otherwise, use the recurrence relation to determine the minimum largest subarray sum
-            # between curr_index and the end of the array with subarray_count subarrays remaining.
-            minimum_largest_split_sum = prefix_sum[n]
-            for i in range(curr_index, n - subarray_count + 1):
-                # Store the sum of the first subarray.
-                first_split_sum = prefix_sum[i + 1] - prefix_sum[curr_index]
-
-                # Find the maximum subarray sum for the current first split.
-                largest_split_sum = max(first_split_sum, 
-                                        get_min_largest_split_sum(i + 1, subarray_count - 1))
-
-                # Find the minimum among all possible combinations.
-                minimum_largest_split_sum = min(minimum_largest_split_sum, largest_split_sum)
-
-                if first_split_sum >= minimum_largest_split_sum:
+        presum = list(accumulate(nums))
+        dp = [[-1]*(K+1) for _ in range(n)]
+        def dfs(i,k): # i....n-1,  minimized largest sum of the split, 
+            if k==1:
+                return presum[n-1]-(presum[i-1] if i>0 else 0)
+            if dp[i][k]!=-1:
+                return dp[i][k]
+            # find the cut from i....n-1
+            ret = presum[n-1]
+            for j in range(n-k+1):
+                cur = presum[j]-(presum[i-1] if i>0 else 0)
+                largest = max(cur, dfs(j+1, k-1))
+                ret = min(ret, largest)
+                if cur >= ret:
                     break
-            
-            return minimum_largest_split_sum
-        
-        return get_min_largest_split_sum(0, m)
+            dp[i][k] = ret
+            return ret
+
+        return dfs(0, K)
