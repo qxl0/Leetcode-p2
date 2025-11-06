e9342c065e74b2c303a4695dfb1faac78a0fb890
qxl0 <qxl0@yahoo.com>
2025-11-05 20:58:30 -0500

    Time: 1635 ms (10.42%), Space: 18.6 MB (9.34%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -2,22 +2,20 @@ class Solution:
     def splitArray(self, nums: List[int], K: int) -> int:
         n = len(nums)
         presum = list(accumulate(nums))
-        dp = [[-1]*(K+1) for _ in range(n)]
-        def dfs(i,k): # i....n-1,  minimized largest sum of the split, 
-            if k==1:
-                return presum[n-1]-(presum[i-1] if i>0 else 0)
-            if dp[i][k]!=-1:
-                return dp[i][k]
-            # find the cut from i....n-1
-            ret = presum[n-1]
-            for j in range(n-k+1):
-                cur = presum[j]-(presum[i-1] if i>0 else 0)
-                if cur >= ret:
-                    break
-                largest = max(cur, dfs(j+1, k-1))
-                ret = min(ret, largest)
-                
-            dp[i][k] = ret
-            return ret
+        dp = [[0]*(K+1) for _ in range(n)]
+        for k in range(1,K+1):
+            for i in range(n):
+                if k == 1:
+                    dp[i][k] = presum[n-1]-(presum[i-1] if i>0 else 0)
+                    continue
+                mn = presum[n-1]
+                for j in range(i,n-k+1):
+                    cur = presum[j]-(presum[i-1] if i>0 else 0)
+                    mx = max(cur, dp[j+1][k-1])
+                    mn = min(mn, mx)
+                    if cur >=mn:
+                        break
+                dp[i][k] = mn
+        return dp[0][K]
 
-        return dfs(0, K)
+                    
