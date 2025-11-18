e17a5c0d450aa5c01939dd1483a2924d66a05ffd
qxl0 <qxl0@yahoo.com>
2025-11-17 20:39:03 -0500

    Time: 0 ms (100%), Space: 21.3 MB (76.08%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def suc(node):
+        def suc(node):            
             node = node.right
             while node.left:
                 node = node.left
@@ -16,20 +16,19 @@ class Solution:
             while node.right:
                 node = node.right
             return node.val
-        if not root: 
+        if not root:
             return None
-        if key<root.val:
-            root.left = self.deleteNode(root.left,key)
-        elif key>root.val:
-            root.right = self.deleteNode(root.right,key)
+        if root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
         else:
-            # root.val == key
             if not root.left and not root.right:
                 return None
-            elif root.right:  # has right child
+            elif root.right:
                 root.val = suc(root)
-                root.right = self.deleteNode(root.right,root.val)
-            else: # has left child
+                root.right = self.deleteNode(root.right, root.val)
+            elif root.left:
                 root.val = pre(root)
                 root.left = self.deleteNode(root.left, root.val)
         return root
