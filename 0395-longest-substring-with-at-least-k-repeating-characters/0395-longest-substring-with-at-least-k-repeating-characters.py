b4b9b08e4c137268a9524862429bead718cb2efd
qxl0 <qxl0@yahoo.com>
2025-12-03 20:33:55 -0500

    Time: 91 ms (26.25%), Space: 17.7 MB (87.36%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
@@ -1,17 +1,26 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        count = Counter(s)
-        n = len(s)
-        if min(count.values())>=k:
-            return n
-        if max(count.values())<k:
-            return 0
+        def helper(s,m,k):
+            # longest substring m different chars 
+            Map = defaultdict(int)
+            j = 0
+            ret = 0
+            count = 0
+            for i in range(len(s)):
+                while j<len(s) and len(Map)<=m:
+                    Map[s[j]]+=1
+                    if Map[s[j]]==k:
+                        count += 1
+                    j += 1
+                    if len(Map)==m and count==m:
+                        ret = max(ret, j-i)
+                Map[s[i]] -= 1
+                if Map[s[i]]==k-1:
+                    count -= 1
+                if Map[s[i]]==0:
+                    del Map[s[i]]
+            return ret
         ret = 0
-        for i in range(n):
-            if count[s[i]]<k:
-                continue
-            j = i
-            while j<n and count[s[j]]>=k:
-                j += 1
-            ret = max(ret, self.longestSubstring(s[i:j],k))
+        for m in range(1,27):
+            ret = max(ret, helper(s,m,k))
         return ret
