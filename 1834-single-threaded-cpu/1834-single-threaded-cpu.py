b90887a8c1196b5a3d0f97013c330f912e6f34af
qxl0 <qxl0@yahoo.com>
2025-12-04 21:08:58 -0500

    Time: 299 ms (86.97%), Space: 57.8 MB (64.91%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -3,22 +3,18 @@ class Solution:
         ret = []
         tt = [(t,dur,id) for id, (t,dur) in enumerate(tasks)]
         tt.sort()
-        q = []
-        heapify(q)
+        q = []        
         cur = tt[0][0]
         i = 0
         while i<len(tt) or q:
-            while i<len(tt) and tt[i][0]<=cur:
-                t,dur,id = tt[i]
-                heappush(q,(dur,id, t))
-                i += 1  
-            if not q: break                      
-            d,id,s = heappop(q)
-            if cur>=s:
-                cur += dur
-                ret.append(id)
-            else:
-                cur = s+dur
-                ret.append(id)
+            if not q and cur<tt[i][0]:
+                cur = tt[i][0]
+            while i<len(tt) and tt[i][0]<=cur:                
+                heappush(q,(tt[i][1], tt[i][2]))
+                i += 1                                  
+            d,id = heappop(q)            
+            cur += d
+            ret.append(id)
+            
                 
         return ret
