3abf26f024d0941fd16a0b875d51773d9c0dc39e
qxl0 <qxl0@yahoo.com>
2025-12-09 20:06:27 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -1,34 +1,26 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         q = []
-        if a > 0:
-            heapq.heappush(q,(-a, 'a'))
-        if b > 0:
-            heapq.heappush(q,(-b, 'b'))
-        if c > 0:
-            heapq.heappush(q,(-c, 'c'))
-        
-        
+        for x,xc in [(a,'a'),(b,'b'),(c,'c')]:
+            if x>0: 
+                heappush(q,(-x,xc))
         ans = ''
-        while q:
-            if len(q) == 1:
-                z,zc = heapq.heappop(q)
-                k = min(-z, 2)
-                ans += zc*k
-                return ans
-            x,xc = heapq.heappop(q)
-            y,yc = heapq.heappop(q)
-            
-            k = min(2, 1+(-x)-(-y))
+        while len(q)>=2:
+            x,xc = heappop(q)
+            y,yc = heappop(q)
+            k = min(2, -x)
             ans += xc*k
             ans += yc
-            
-            nx = (-x)-k
-            ny = (-y)-1
-            if nx>0:
-                heapq.heappush(q, (-nx, xc))
-            if ny>0:
-                heapq.heappush(q, (-ny, yc))
-            # print(ans)
+            if -x>k:
+                heappush(q, (x+k,xc))
+            if -y>1:
+                heappush(q, (y+1,yc))
+        if q:
+            x,xc = heappop(q)
+            if -x>=3:
+                ans += xc*2
+            else:
+                ans += xc*(-x)
+        
         return ans
-            
+        
