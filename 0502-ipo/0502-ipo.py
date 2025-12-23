c9d471fd47bc41b9472d1512c7ea2bfea10c25b6
qxl0 <qxl0@yahoo.com>
2025-12-22 20:18:20 -0500

    Time: 295 ms (56.62%), Space: 44.3 MB (9.09%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,16 +1,18 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        heap1,heap2 = [(c,p) for p,c in zip(profits,capital)], []
-        heapify(heap1)
-        
-        while k>0:
-            while len(heap1)>0 and heap1[0][0]<=w:
-                c,p = heappop(heap1)
-                heappush(heap2, (-p, c))
-            if len(heap2)>0:
-                p,_ = heappop(heap2)
-                w += -p
-                k -= 1
-            else:
+        arr = [(c,p) for c,p in zip(capital, profits)]
+        arr.sort()
+        hq = []
+        ret = w
+        i = 0
+        while k:
+            while i<len(arr) and arr[i][0]<=w:
+                heappush(hq, -arr[i][1])
+                i += 1
+            if not hq:
                 break
-        return w
+            p = heappop(hq)
+            ret += (-p)
+            w += (-p)
+            k -= 1
+        return ret 
