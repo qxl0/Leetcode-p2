09a44fdc7e0e42b20835a7fb5d4e4edc7702ffb6
qxl0 <qxl0@yahoo.com>
2025-12-26 15:31:38 -0500

    Time: 140 ms (53.25%), Space: 78 MB (39.17%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,23 +1,60 @@
+class Node:
+    def __init__(self, key,val):
+        self.key = key
+        self.val = val
+        self.next = self.prev = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.data = OrderedDict()
-        self.capacity = capacity
+        self.cap = capacity
+        self.Map = {} # key -> Node
+        self.left,self.right = Node(0,0),Node(0,0) # left: LRU, right: most recently visited
+        self.left.next = self.right  
+        self.right.prev = self.left
 
+    def remove(self, node):
+        # 
+        k = node.key
+        if k not in self.Map:
+            return 
+        # in Map 
+        del self.Map[k]
+        # in duble linked list 
+        p = node.prev 
+        n = node.next 
+        p.next = n 
+        n.prev = p
+    def add(self,node):
+        # add to head 
+        temp = self.right.prev
+        temp.next = node 
+        node.next = self.right        
+        node.prev = temp
+        self.right.prev = node  
+        self.Map[node.key] = node 
     def get(self, key: int) -> int:
-        if key not in self.data: return -1
-        ret = self.data.pop(key)
-        self.data[key] = ret  # add it to the end, most recently used
-        return ret
+        if key not in self.Map:
+            return -1
+        cur = self.Map[key]
+        self.remove(cur)
+        self.add(cur)
+        return cur.val
 
     def put(self, key: int, value: int) -> None:
-        if key in self.data: # already in cache
-            self.data.pop(key)
-        if len(self.data)==self.capacity:
-            # remove first one
-            self.data.popitem(last=False)  # remove first one
+        if key in self.Map:
+            cur = self.Map[key]
+            cur.val = value
+            self.remove(cur)             
+        else:
+            cur = Node(key, value)
+        # check if capcacity 
+        if len(self.Map)>=self.cap:
+            self.remove(self.left.next)            
+        self.add(cur)
+        self.Map[key] = cur
         
-        self.data[key] = value
+
 
 
 # Your LRUCache object will be instantiated and called as such:
