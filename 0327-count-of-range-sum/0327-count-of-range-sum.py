98411e243fb9ddd1d95b76f2679bca5924a6ef1f
qxl0 <qxl0@yahoo.com>
2026-02-28 17:11:13 -0500

    Time: 6608 ms (5.01%), Space: 28.1 MB (99.87%) - LeetHub

--- 0327-count-of-range-sum/0327-count-of-range-sum.py
+++ 0327-count-of-range-sum/0327-count-of-range-sum.py
@@ -1,49 +1,47 @@
-class FenwickTree:
-    def __init__(self,n):
-        self.n = n
-        self.data = [0]*(n+1)
-    def lowbit(self,x):
-        return x & (-x)
-    def update(self,i,t):
-        while i <= self.n:
-            self.data[i] += t
-            i += self.lowbit(i)
-    def query(self,i):
-        ret = 0
-        while i>0:
-            ret += self.data[i]
-            i -= self.lowbit(i)
-        return ret
-    def rangesum(self, l, r):
-        if l>r: return 0
-        return self.query(r)-self.query(l-1)
+from bisect import bisect, bisect_left
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
-        '''
-        lower<=P[i]-P[j]<=upper
-        P[i]-upper<=P[j]<=P[i]-lower
-        scan P[i], find P[j] in range [P[i]-upper, P[i]-lower]
-        '''
-        n = len(nums)        
-        # 1. get presum
-        P = [0]*(n+1)
+        n = len(nums)
+        presum = [0]*(n+1)
         for i in range(n):
-            P[i+1] = P[i] + nums[i]
-        # 2. Coordinate compression over values
-        vals = []
-        for v in P:
-            vals.append(v)
-            vals.append(v-lower)
-            vals.append(v-upper)
-        Map = {x:(i+1) for i,x in enumerate(sorted(set(vals)))}
-        # x=>index from 1
-        N = len(Map)
-        tree = FenwickTree(N)
-        tree.update(Map[P[0]], 1)
-        ans = 0
-        for i in range(1,n+1):
-            k = Map[P[i]]
-            print(f'{i}: {k-upper}, {k-lower}')
-            ans += tree.rangesum(Map[P[i]-upper], Map[P[i]-lower])
-            tree.update(k, 1)
-        return ans
+            presum[i+1] = presum[i]+nums[i]
+        
+        def helper(presum, a, b, lower,upper):
+            nonlocal ret
+            if a>=b:return
+            mid = a + (b-a)//2
+            helper(presum, a, mid, lower, upper)
+            helper(presum, mid+1, b, lower, upper)
+            x,y = 0,0
+            for i in range(a, mid+1):
+                x = bisect_left(presum, presum[i]+lower, mid+1, b+1)
+                y = bisect(presum, presum[i]+upper, mid+1, b+1)
+                ret += y-x
+            
+            temp = [0]*(b-a+1)
+            p = 0
+            i = a
+            j = mid+1
+            while i<=mid and j<=b:
+                if presum[i]<=presum[j]:
+                    temp[p] = presum[i]
+                    i += 1
+                else:
+                    temp[p] = presum[j]
+                    j += 1
+                p += 1
+            while i<=mid:
+                temp[p] = presum[i]
+                i += 1
+                p += 1
+            while j<=b:
+                temp[p] = presum[j]
+                j += 1
+                p += 1
+            for i in range(b-a+1):
+                presum[i+a] = temp[i]
+            
+        ret = 0
+        helper(presum, 0, n, lower, upper)
+        
+        return ret
