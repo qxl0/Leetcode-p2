a5fa201d820836f2e0cdc9ad38957baa83f28070
qxl0 <qxl0@yahoo.com>
2026-04-01 20:30:34 -0400

    N-Queens II

---
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        m, n = len(rooms),len(rooms[0])
+        EMPTY = 2**31 - 1
+        gates = [(0, i,j) for i in range(m) for j in range(n) if rooms[i][j]==0]
+        
+        while gates:
+            d, x, y = gates.pop(0)
+            
+            rooms[x][y] = min(rooms[x][y], d)
+            for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                nx,ny = x+dx, y+dy
+                if nx<0 or nx>=m or ny<0 or ny>=n: continue
+                if rooms[nx][ny] == EMPTY:
+                    gates.append((d+1, nx, ny))
+        
